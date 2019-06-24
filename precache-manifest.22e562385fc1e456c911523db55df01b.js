d819fb357e372ff06e3360c8636f6d451c9ff408
BMidhun <midhunbabygeorge@gmail.com>
2019-06-25 02:07:16 +0530

    Updates

---
+++ precache-manifest.22e562385fc1e456c911523db55df01b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4178506553ba11a9fdd713526212b87",
+    "url": "/myreact-calculator/index.html"
+  },
+  {
+    "revision": "cd9e99159bc551b7c6d2",
+    "url": "/myreact-calculator/static/css/main.1e06afaf.chunk.css"
+  },
+  {
+    "revision": "f5850cce42cdca00f7d7",
+    "url": "/myreact-calculator/static/js/2.27b37548.chunk.js"
+  },
+  {
+    "revision": "cd9e99159bc551b7c6d2",
+    "url": "/myreact-calculator/static/js/main.908b6cfa.chunk.js"
+  },
+  {
+    "revision": "9e95f26a623bf179ce5f",
+    "url": "/myreact-calculator/static/js/runtime~main.8423f79e.js"
+  }
+]);
